789fc3e696a106405aafc976960c825bad8c858b
Nisarg Trivedi <nisarg.trivedi@exploredigits.com>
2026-09-11 23:08:06 -0400

    Configure Firebase project

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,11 @@
 window.PICKLEBALL_FIREBASE_CONFIG = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyCBnTb5-7I5zVW-3xao1ZszkYpuQSiGkLs",
+  authDomain: "pickleball-2d07a.firebaseapp.com",
+  projectId: "pickleball-2d07a",
+  storageBucket: "pickleball-2d07a.firebasestorage.app",
+  messagingSenderId: "712675683186",
+  appId: "1:712675683186:web:8a050cf1f231d64ec88beb",
+  measurementId: "G-X8W91EV6S3"
 };
 
 window.PICKLEBALL_TOURNAMENT_ID = "main";
